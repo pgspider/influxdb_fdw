9815b51fa2feaafd713b477ec2441c43a3bb4754
Nguyen The Son <son.nguyenthe@toshiba-tsdv.com>
2020-03-15 08:37:22 +0700

    Note: Influxdb does not support group by clause. https://tccloud2.toshiba.co.jp/accio/redmine/issues/442
    update limit.sql file
    update limit.out file
    
    Signed-off-by: Nguyen The Son <son.nguyenthe@toshiba-tsdv.com>

--- sql/extra/limit.sql
+++ sql/extra/limit.sql
@@ -184,12 +184,14 @@ select generate_series(0,2) as s1, generate_series((random()*.1)::int,2) as s2
 order by s2 desc;
 
 -- test for failure to set all aggregates' aggtranstype
-explain (verbose, costs off)
-select sum(tenthous) as s1, sum(tenthous) + random()*0 as s2
-  from tenk1 group by thousand order by thousand limit 3;
 
-select sum(tenthous) as s1, sum(tenthous) + random()*0 as s2
-  from tenk1 group by thousand order by thousand limit 3;
+-- influxdb does not support group by clause.
+-- explain (verbose, costs off)
+-- select sum(tenthous) as s1, sum(tenthous) + random()*0 as s2
+--   from tenk1 group by thousand order by thousand limit 3;
+
+-- select sum(tenthous) as s1, sum(tenthous) + random()*0 as s2
+--   from tenk1 group by thousand order by thousand limit 3;
 
 -- Clean up
 DO $d$
