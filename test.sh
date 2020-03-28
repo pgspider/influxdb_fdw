c836c2f536311634a083d133c66f65d0fd0b1074
Duong Ngoc Lam <lam.duongngoc@toshiba-tsdv.com>
2020-03-28 14:31:44 +0700

    add test shell script
    Signed-off-by: Duong Ngoc Lam <lam.duongngoc@toshiba-tsdv.com>

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
-export USE_PGXS=1
-./init.sh && make clean && make && make install && make installcheck
+#export USE_PGXS=1
+sed -i 's/REGRESS =.*/REGRESS = aggregate influxdb_fdw/' Makefile
+
+./init.sh && make clean && make && make install && make check
