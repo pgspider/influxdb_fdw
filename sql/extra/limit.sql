4bdae228a7960a6bd1a48e588c7738fc82f6f0f0
minhla <minh1.leanh@toshiba.co.jp>
2020-06-30 09:09:40 +0000

    Add test case ID for influxdb_fdw test

--- sql/extra/limit.sql
+++ sql/extra/limit.sql
@@ -52,78 +52,114 @@ CREATE FOREIGN TABLE tenk1 (
 	string4	 	name
 ) SERVER influxdb_svr OPTIONS (table 'tenk');
 
+--Testcase 1:
 SELECT ''::text AS two, unique1, unique2, stringu1
 		FROM onek WHERE unique1 > 50
 		ORDER BY unique1 LIMIT 2;
+--Testcase 2:
 SELECT ''::text AS five, unique1, unique2, stringu1
 		FROM onek WHERE unique1 > 60
 		ORDER BY unique1 LIMIT 5;
+--Testcase 3:
 SELECT ''::text AS two, unique1, unique2, stringu1
 		FROM onek WHERE unique1 > 60 AND unique1 < 63
 		ORDER BY unique1 LIMIT 5;
+--Testcase 4:
 SELECT ''::text AS three, unique1, unique2, stringu1
 		FROM onek WHERE unique1 > 100
 		ORDER BY unique1 LIMIT 3 OFFSET 20;
+--Testcase 5:
 SELECT ''::text AS zero, unique1, unique2, stringu1
 		FROM onek WHERE unique1 < 50
 		ORDER BY unique1 DESC LIMIT 8 OFFSET 99;
+--Testcase 6:
 SELECT ''::text AS eleven, unique1, unique2, stringu1
 		FROM onek WHERE unique1 < 50
 		ORDER BY unique1 DESC LIMIT 20 OFFSET 39;
+--Testcase 7:
 SELECT ''::text AS ten, unique1, unique2, stringu1
 		FROM onek
 		ORDER BY unique1 OFFSET 990;
+--Testcase 8:
 SELECT ''::text AS five, unique1, unique2, stringu1
 		FROM onek
 		ORDER BY unique1 OFFSET 990 LIMIT 5;
+--Testcase 9:
 SELECT ''::text AS five, unique1, unique2, stringu1
 		FROM onek
 		ORDER BY unique1 LIMIT 5 OFFSET 900;
 
 -- Test null limit and offset.  The planner would discard a simple null
 -- constant, so to ensure executor is exercised, do this:
+--Testcase 10:
 select * from int8_tbl limit (case when random() < 0.5 then null::bigint end);
+--Testcase 11:
 select * from int8_tbl offset (case when random() < 0.5 then null::bigint end);
 
 -- Test assorted cases involving backwards fetch from a LIMIT plan node
 begin;
 
 declare c1 scroll cursor for select * from int8_tbl limit 10;
+--Testcase 12:
 fetch all in c1;
+--Testcase 13:
 fetch 1 in c1;
+--Testcase 14:
 fetch backward 1 in c1;
+--Testcase 15:
 fetch backward all in c1;
+--Testcase 16:
 fetch backward 1 in c1;
+--Testcase 17:
 fetch all in c1;
 
 declare c2 scroll cursor for select * from int8_tbl limit 3;
+--Testcase 18:
 fetch all in c2;
+--Testcase 19:
 fetch 1 in c2;
+--Testcase 20:
 fetch backward 1 in c2;
+--Testcase 21:
 fetch backward all in c2;
+--Testcase 22:
 fetch backward 1 in c2;
+--Testcase 23:
 fetch all in c2;
 
 declare c3 scroll cursor for select * from int8_tbl offset 3;
+--Testcase 24:
 fetch all in c3;
+--Testcase 25:
 fetch 1 in c3;
+--Testcase 26:
 fetch backward 1 in c3;
+--Testcase 27:
 fetch backward all in c3;
+--Testcase 28:
 fetch backward 1 in c3;
+--Testcase 29:
 fetch all in c3;
 
 declare c4 scroll cursor for select * from int8_tbl offset 10;
+--Testcase 30:
 fetch all in c4;
+--Testcase 31:
 fetch 1 in c4;
+--Testcase 32:
 fetch backward 1 in c4;
+--Testcase 33:
 fetch backward all in c4;
+--Testcase 34:
 fetch backward 1 in c4;
+--Testcase 35:
 fetch all in c4;
 
 rollback;
 
 -- Stress test for variable LIMIT in conjunction with bounded-heap sorting
 
+--Testcase 36:
 SELECT
   (SELECT n
      FROM (VALUES (1)) AS x,
@@ -138,48 +174,62 @@ SELECT
 
 create temp sequence testseq;
 
+--Testcase 37:
 explain (verbose, costs off)
 select unique1, unique2, nextval('testseq')
   from tenk1 order by unique2 limit 10;
 
+--Testcase 38:
 select unique1, unique2, nextval('testseq')
   from tenk1 order by unique2 limit 10;
 
+--Testcase 39:
 select currval('testseq');
 
+--Testcase 40:
 explain (verbose, costs off)
 select unique1, unique2, nextval('testseq')
   from tenk1 order by tenthous limit 10;
 
+--Testcase 41:
 select unique1, unique2, nextval('testseq')
   from tenk1 order by tenthous limit 10;
 
+--Testcase 42:
 select currval('testseq');
 
+--Testcase 43:
 explain (verbose, costs off)
 select unique1, unique2, generate_series(1,10)
   from tenk1 order by unique2 limit 7;
 
+--Testcase 44:
 select unique1, unique2, generate_series(1,10)
   from tenk1 order by unique2 limit 7;
 
+--Testcase 45:
 explain (verbose, costs off)
 select unique1, unique2, generate_series(1,10)
   from tenk1 order by tenthous limit 7;
 
+--Testcase 46:
 select unique1, unique2, generate_series(1,10)
   from tenk1 order by tenthous limit 7;
 
 -- use of random() is to keep planner from folding the expressions together
+--Testcase 47:
 explain (verbose, costs off)
 select generate_series(0,2) as s1, generate_series((random()*.1)::int,2) as s2;
 
+--Testcase 48:
 select generate_series(0,2) as s1, generate_series((random()*.1)::int,2) as s2;
 
+--Testcase 49:
 explain (verbose, costs off)
 select generate_series(0,2) as s1, generate_series((random()*.1)::int,2) as s2
 order by s2 desc;
 
+--Testcase 50:
 select generate_series(0,2) as s1, generate_series((random()*.1)::int,2) as s2
 order by s2 desc;
 
--- sql/extra/prepare.sql
+++ sql/extra/prepare.sql
@@ -34,80 +34,106 @@ CREATE FOREIGN TABLE road (
 	thepath 	path
 ) SERVER influxdb_svr;
 
+--Testcase 1:
 SELECT name, statement, parameter_types FROM pg_prepared_statements;
 
+--Testcase 2:
 PREPARE q1 AS SELECT 1 AS a;
+--Testcase 3:
 EXECUTE q1;
 
+--Testcase 4:
 SELECT name, statement, parameter_types FROM pg_prepared_statements;
 
 -- should fail
+--Testcase 5:
 PREPARE q1 AS SELECT 2;
 
 -- should succeed
 DEALLOCATE q1;
+--Testcase 6:
 PREPARE q1 AS SELECT 2;
+--Testcase 7:
 EXECUTE q1;
 
+--Testcase 8:
 PREPARE q2 AS SELECT 2 AS b;
+--Testcase 9:
 SELECT name, statement, parameter_types FROM pg_prepared_statements;
 
 -- sql92 syntax
 DEALLOCATE PREPARE q1;
 
+--Testcase 10:
 SELECT name, statement, parameter_types FROM pg_prepared_statements;
 
 DEALLOCATE PREPARE q2;
 -- the view should return the empty set again
+--Testcase 11:
 SELECT name, statement, parameter_types FROM pg_prepared_statements;
 
 -- parameterized queries
+--Testcase 12:
 PREPARE q2(text) AS
 	SELECT datname, datistemplate, datallowconn
 	FROM pg_database WHERE datname = $1;
 
+--Testcase 13:
 EXECUTE q2('postgres');
 
+--Testcase 14:
 PREPARE q3(text, int, float, boolean, smallint) AS
 	SELECT * FROM tenk1 WHERE string4 = $1 AND (four = $2 OR
 	ten = $3::bigint OR true = $4 OR odd = $5::int)
 	ORDER BY unique1;
 
+--Testcase 15:
 EXECUTE q3('AAAAxx', 5::smallint, 10.5::float, false, 4::bigint);
 
 -- too few params
+--Testcase 16:
 EXECUTE q3('bool');
 
 -- too many params
+--Testcase 17:
 EXECUTE q3('bytea', 5::smallint, 10.5::float, false, 4::bigint, true);
 
 -- wrong param types
+--Testcase 18:
 EXECUTE q3(5::smallint, 10.5::float, false, 4::bigint, 'bytea');
 
 -- invalid type
+--Testcase 19:
 PREPARE q4(nonexistenttype) AS SELECT $1;
 
 -- create table as execute
+--Testcase 20:
 PREPARE q5(int, text) AS
 	SELECT * FROM tenk1 WHERE unique1 = $1 OR stringu1 = $2
 	ORDER BY unique1;
 CREATE TEMPORARY TABLE q5_prep_results AS EXECUTE q5(200, 'DTAAAA');
+--Testcase 21:
 SELECT * FROM q5_prep_results;
 CREATE TEMPORARY TABLE q5_prep_nodata AS EXECUTE q5(200, 'DTAAAA')
     WITH NO DATA;
+--Testcase 22:
 SELECT * FROM q5_prep_nodata;
 
 -- unknown or unspecified parameter types: should succeed
+--Testcase 23:
 PREPARE q6 AS
     SELECT * FROM tenk1 WHERE unique1 = $1 AND stringu1 = $2;
+--Testcase 24:
 PREPARE q7(unknown) AS
     SELECT * FROM road WHERE thepath = $1;
 
+--Testcase 25:
 SELECT name, statement, parameter_types FROM pg_prepared_statements
     ORDER BY name;
 
 -- test DEALLOCATE ALL;
 DEALLOCATE ALL;
+--Testcase 26:
 SELECT name, statement, parameter_types FROM pg_prepared_statements
     ORDER BY name;
 
